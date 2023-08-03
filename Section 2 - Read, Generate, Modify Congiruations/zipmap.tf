ce5eefd4f62bdd2cc87cc087ed2d55ee010c4032
Ahad Noor Shakti <53283457+ShaktiAhad@users.noreply.github.com>
2023-08-03 12:33:13 +0900

    Update zipmap.tf

--- Section 2 - Read, Generate, Modify Congiruations/zipmap.tf
+++ Section 2 - Read, Generate, Modify Congiruations/zipmap.tf
@@ -1,3 +1,4 @@
+### using count
 provider "aws" {
   region     = "us-west-2"
   access_key = "YOUR-ACCESS-KEY"
@@ -16,7 +17,20 @@ output "arns" {
   value = aws_iam_user.lb[*].arn
 }
 
-
 output "zipmap" {
   value = zipmap(aws_iam_user.lb[*].name, aws_iam_user.lb[*].arn)
+}
+
+
+### using for_each [https://developer.hashicorp.com/terraform/language/functions/values]
+resource "aws_iam_user" "iam_users" {
+  for_each = toset(var.iam_user_list)
+  name = each.key
+
+  tags = {
+    tag-key = "${each.key}_tf_iam_user"
+  }
+}
+output "iam_users_arn" {
+  value = zipmap(values(aws_iam_user.iam_users)[*].name, values(aws_iam_user.iam_users)[*].arn)
 }
